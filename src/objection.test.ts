57020d6a1f8599c38cf2bd90568f22575d4e5742
Anson Lai <z5361987@ad.unsw.edu.au>
2022-07-14 23:16:04 +1000

    done

--- src/objection.test.ts
+++ src/objection.test.ts
@@ -1,10 +1,21 @@
 import { getObjections, Objection, ExaminationType } from './objection';
 
-test('Remove this test and uncomment the tests below', () => {
-  expect(1 + 1).toBe(2);
+describe('error', () => {
+  test.each([
+    {
+      question: '',
+      testimony: 'No you!',
+      type: ExaminationType.CROSS,
+    },
+    {
+      question: 'This is direct, yes!',
+      testimony: '',
+      type: ExaminationType.DIRECT,
+    },
+  ])('$objections', ({ question, testimony, type }) => {
+    expect(() => getObjections(question, testimony, type)).toThrow(Error);
+  });
 });
-
-/*
 
 describe('argumentative cases', () => {
   test.each([
@@ -25,4 +36,158 @@ describe('argumentative cases', () => {
   });
 });
 
-*/
+describe('compound cases', () => {
+  test.each([
+    {
+      question: 'You are totally lying??',
+      testimony: 'No you!',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.COMPOUND]),
+    },
+    {
+      question: 'How are you? You are totally lying?',
+      testimony: 'Yes! you are',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.COMPOUND]),
+    },
+    {
+      question: 'This is direct, yes!',
+      testimony: 'Yes, so not argumentative!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('hearsay cases', () => {
+  test.each([
+    {
+      question: 'You are totally lying!',
+      testimony: 'heard from someone you.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.HEARSAY]),
+    },
+    {
+      question: 'This is direct, yes!',
+      testimony: 'she told me yes.',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.HEARSAY]),
+    },
+    {
+      question: 'This is direct, yes!',
+      testimony: 'Yes!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('leading cases', () => {
+  test.each([
+    {
+      question: 'Why did you do that?',
+      testimony: 'No you!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'Do you agree?',
+      testimony: 'Yes, so do!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'This is direct, right?',
+      testimony: 'No is!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'This is direct, correct?',
+      testimony: 'No is!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.LEADING]),
+    },
+    {
+      question: 'This is direct?',
+      testimony: 'No is!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+    {
+      question: 'This is direct, correct?',
+      testimony: 'No is!',
+      type: ExaminationType.CROSS,
+      objections: new Set([]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('NonResponsive cases', () => {
+  test.each([
+    {
+      question: 'This is direct, yes!',
+      testimony: 'So not argumentative!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.NON_RESPONSIVE]),
+    },
+    {
+      question: 'You are totally lying!',
+      testimony: 'No you!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('Relevance cases', () => {
+  test.each([
+    {
+      question: 'You are totally lying!',
+      testimony: 'No you!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+    {
+      question: 'This!',
+      testimony: 'Yes, this not argumentative!',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.RELEVANCE]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
+
+describe('Speculation cases', () => {
+  test.each([
+    {
+      question: 'I think you are totally lying?',
+      testimony: 'No you!',
+      type: ExaminationType.CROSS,
+      objections: new Set([Objection.SPECULATION]),
+    },
+    {
+      question: 'This so?',
+      testimony: 'Yes, so think !',
+      type: ExaminationType.DIRECT,
+      objections: new Set([Objection.SPECULATION]),
+    },
+    {
+      question: 'This so!',
+      testimony: 'Yes, so !',
+      type: ExaminationType.DIRECT,
+      objections: new Set([]),
+    },
+  ])('$objections', ({ question, testimony, type, objections }) => {
+    expect(getObjections(question, testimony, type)).toEqual(objections);
+  });
+});
--- src/objection.ts
+++ src/objection.ts
@@ -28,6 +28,64 @@ function isArgumentative(question: string) {
   return !question.endsWith('?');
 }
 
+function isCompound(question: string) {
+  let pos = question.indexOf('?');
+  if (pos === -1) {
+    return false;
+  }
+  let str = question.substring(0, pos) + question.substring(pos+1, question.length);
+  console.log(str)
+  if (str.indexOf("?") === -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isHearsay(testimony: string) {
+  if (testimony.includes('heard from') || testimony.includes('told me')) {
+    return true;
+  }
+  return false;
+}
+
+function isLeading(question: string) {
+  if (question.startsWith('why did you') || question.startsWith('do you agree') ||
+    question.endsWith('right?') || question.endsWith('correct?')) {
+      return true;
+  } 
+  return false;
+}
+
+function isNonResponsive(testimony: string, question: string) {
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const q_result = question.replace(regex, '');
+  const t_result = testimony.replace(regex, '');
+  let q_arr = q_result.split(" ");
+  for (const word of q_arr) {
+    if (testimony.includes(word)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isRelevance(testimony: string, question: string) {
+  let t_length = testimony.length;
+  let q_length = question.length;
+  if (t_length >= 2 * q_length) {
+    return true;
+  }
+  return false;
+}
+
+function isSpeculation(type: ExaminationType, testimony: string, question: string) {
+  if (type === ExaminationType.DIRECT) {
+    return testimony.includes('think');
+  } else {
+    return question.includes('think');
+  }
+}
 /**
  * Feel free to modify the function below as you see fit,
  * so long as you satisfy the specification.
@@ -38,7 +96,9 @@ export function getObjections(
   type: ExaminationType
 ): Set<Objection> {
   // TODO: error handling
-
+  if (question === '' || testimony === '') {
+    throw new Error(' ');
+  }
   // Convert given question and testimony to lowercase
   question = question.toLowerCase();
   testimony = testimony.toLowerCase();
@@ -49,15 +109,33 @@ export function getObjections(
     if (isArgumentative(question)) {
       objections.add(Objection.ARGUMENTATIVE);
     }
-
     // TODO
   } else {
     // Type is ExaminationType.DIRECT
-
+    if (isLeading(question)) {
+      objections.add(Objection.LEADING);
+    }
     // TODO
   }
 
   // TODO
-
+  if (isCompound(question)) {
+    objections.add(Objection.COMPOUND);
+  }
+  if (isHearsay(testimony)) {
+    objections.add(Objection.HEARSAY);
+  }
+  if (isNonResponsive(testimony, question)) {
+    objections.add(Objection.NON_RESPONSIVE);
+  }
+  if (isRelevance(testimony, question)) {
+    objections.add(Objection.RELEVANCE);
+  }
+  if (isSpeculation(type, testimony, question)) {
+    objections.add(Objection.SPECULATION);
+  }
   return objections;
 }
+
+
+//getObjections('', 'No you!', ExaminationType.DIRECT);
